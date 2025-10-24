80c50d52d774541d83a7d2b51372c1a442d7c466
Vlad <vlvete1990@yandex.ru>
2025-10-25 01:07:24 +0300

    feature: add CircleCast isGrounded checker

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -9,6 +9,9 @@ namespace  PixelCrew
         [SerializeField] private float _jumpSpeed;
         [SerializeField] private LayerMask _groundLayer;
         
+        [SerializeField] private float _groundCheckRadius;
+        [SerializeField] private Vector3 _groundCheckPositionDelta;
+        
         private Rigidbody2D _rigidbody;
         
         private Vector2 _direction;
@@ -28,10 +31,22 @@ namespace  PixelCrew
             _rigidbody.linearVelocity = new Vector2(_direction.x * _speed, _rigidbody.linearVelocity.y);
             
             var isJumping = _direction.y > 0;
-            if (isJumping)
+            if (isJumping && IsGrounded())
             {
                 _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
             }
+        }
+
+        private bool IsGrounded()
+        {
+           var hit = Physics2D.CircleCast(transform.position + _groundCheckPositionDelta, _groundCheckRadius, Vector2.down, 0, _groundLayer);
+           return hit.collider != null;
+        }
+
+        private void OnDrawGizmos()
+        { 
+            Gizmos.color = IsGrounded() ? Color.green : Color.red;
+            Gizmos.DrawSphere(transform.position + _groundCheckPositionDelta, _groundCheckRadius);
         }
     }
 };
