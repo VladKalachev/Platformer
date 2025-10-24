25f324ae66735e712f2d375346c913c890a1a0de
Vlad <vlvete1990@yandex.ru>
2025-10-25 00:32:03 +0300

    feature: add base pfysicales

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -6,20 +6,31 @@ namespace  PixelCrew
     public class Hero : MonoBehaviour
     {
         [SerializeField] private float _speed;
-  
+        [SerializeField] private float _jumpSpeed;
+        [SerializeField] private LayerMask _groundLayer;
+        
+        private Rigidbody2D _rigidbody;
+        
         private Vector2 _direction;
-  
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
         public void SetDirection(Vector2 direction)
         {
             _direction = direction;  
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
-            if (_direction.magnitude != 0)
+            _rigidbody.linearVelocity = new Vector2(_direction.x * _speed, _rigidbody.linearVelocity.y);
+            
+            var isJumping = _direction.y > 0;
+            if (isJumping)
             {
-                var delta = _direction * _speed * Time.deltaTime;
-                transform.position = transform.position + new Vector3(delta.x, delta.y, transform.position.z);
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
             }
         }
     }
