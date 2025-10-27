73a83b6c08fd786fec4d2b9bdae56c0f27cebc20
Vlad <vlvete1990@yandex.ru>
2025-10-27 21:25:19 +0300

    feature: add base animation

--- Assets/PixelCrew/Hero.cs
+++ Assets/PixelCrew/Hero.cs
@@ -12,10 +12,19 @@ namespace  PixelCrew
         
         private Rigidbody2D _rigidbody;
         private Vector2 _direction;
+        
+        private Animator _animator;
+        private SpriteRenderer _sprite;
+        
+        private static readonly int IsGround =  Animator.StringToHash("is-ground");
+        private static readonly int IsVerticalVelocity =  Animator.StringToHash("vertical-velocity");
+        private static readonly int IsRunning =  Animator.StringToHash("is-running");
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator =  GetComponent<Animator>();
+            _sprite =  GetComponent<SpriteRenderer>();
         }
 
         public void SetDirection(Vector2 direction)
@@ -23,14 +32,26 @@ namespace  PixelCrew
             _direction = direction;  
         }
 
+        private void UpdateSpriteDirection()
+        {
+            if (_direction.x > 0)
+            {
+                _sprite.flipX = false;
+            } else if (_direction.x < 0)
+            {
+                _sprite.flipX = true;
+            }
+        }
+
         private void FixedUpdate()
         {
             _rigidbody.linearVelocity = new Vector2(_direction.x * _speed, _rigidbody.linearVelocity.y);
             
             var isJumping = _direction.y > 0;
+            var isGrounded = IsGrounded();
             if (isJumping)
             {
-                if (IsGrounded() && _rigidbody.linearVelocity.y <= 0)
+                if (isGrounded && _rigidbody.linearVelocity.y <= 0)
                 {
                     _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);    
                 }
@@ -39,6 +60,12 @@ namespace  PixelCrew
                 _rigidbody.linearVelocity =
                     new Vector2(_rigidbody.linearVelocity.x, _rigidbody.linearVelocity.y * 0.5f);
             }
+            
+            _animator.SetBool(IsGround, isGrounded);
+            _animator.SetBool(IsRunning, _direction.x != 0);
+            _animator.SetFloat(IsVerticalVelocity, _rigidbody.linearVelocity.y);
+            
+            UpdateSpriteDirection();
         }
 
         private bool IsGrounded()
