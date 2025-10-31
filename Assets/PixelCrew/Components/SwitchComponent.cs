1443e4b253cebb248f4f283dda86a1e2a438126d
Vlad <vlvete1990@yandex.ru>
2025-11-01 00:08:39 +0300

    feature: add SwitchComponent

--- Assets/PixelCrew/Components/SwitchComponent.cs
+++ Assets/PixelCrew/Components/SwitchComponent.cs
@@ -4,6 +4,20 @@ namespace PixelCrew.Components
 {
     public class SwitchComponent: MonoBehaviour
     {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private bool _state;
+        [SerializeField] private string _animatorKey;
         
+        public void Switch()
+        {
+            _state = !_state;
+            _animator.SetBool(_animatorKey, _state);
+        }
+
+        [ContextMenu("Switch")]
+        public void SwitchIt()
+        {
+            Switch();
+        }
     }
 }
