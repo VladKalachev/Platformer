a2b919180229f26312a7f15a98b3cd15da5cf21b
Vlad <vlvete1990@yandex.ru>
2025-11-07 00:14:48 +0300

    feature: add animation for TeleportComponent

--- Assets/PixelCrew/Components/TeleportComponent.cs
+++ Assets/PixelCrew/Components/TeleportComponent.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace PixelCrew.Components
@@ -6,10 +7,55 @@ namespace PixelCrew.Components
     public class TeleportComponent : MonoBehaviour
     {
         [SerializeField] private Transform _destTransform;
+        [SerializeField] private float _alphaTime = 1;
+        [SerializeField] private float _moveTime = 1;
 
         public void Teleport(GameObject target)
         {
-            target.transform.position = _destTransform.position;
+            StartCoroutine(AnimationTeleport(target));
+        }
+
+        private IEnumerator AnimationTeleport(GameObject target)
+        {
+            var sprite = target.GetComponent<SpriteRenderer>();
+
+            yield return AlphaAnimation(sprite, 0);
+            target.SetActive(false);
+
+            yield return MoveAnimation(target);
+          
+            target.SetActive(true);
+            yield return AlphaAnimation(sprite, 1);
+        }
+
+        private IEnumerator MoveAnimation(GameObject target)
+        {
+            var moveTime = 0f;
+            while (moveTime < _moveTime)
+            {
+                moveTime += Time.deltaTime;
+                var progress = moveTime / _moveTime;
+                target.transform.position = Vector3.Lerp(target.transform.position, _destTransform.position, progress);
+                
+                yield return null;
+            }
+        }
+
+        private IEnumerator AlphaAnimation(SpriteRenderer sprite, float destAlpha)
+        {
+            var alphaTime = 0f;
+            var spriteAlpha = sprite.color.a;
+            while (alphaTime < _alphaTime) 
+            {
+                alphaTime += Time.deltaTime;
+                var progress = alphaTime / _alphaTime;
+                var tmpAlpha = Mathf.Lerp(spriteAlpha, destAlpha, progress);
+                var color = sprite.color;
+                color.a = tmpAlpha;
+                sprite.color = color;
+                
+                yield return null;
+            }
         }
     }
 }
