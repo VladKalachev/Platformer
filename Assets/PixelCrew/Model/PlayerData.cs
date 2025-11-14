efdbbea2278f689fe8c66ced2d351d32a35852ca
Vlad <vlvete1990@yandex.ru>
2025-11-15 02:12:20 +0300

    feature: add Player Data

---
+++ Assets/PixelCrew/Model/PlayerData.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PixelCrew.Model
+{
+    [Serializable]
+    public class PlayerData
+    {
+        public int Coins;
+        public int Hp;
+        public int IsArmed;
+    }
+}
