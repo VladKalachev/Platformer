192480cc8561f57d1533910084bb7c53cd90bfce
Vlad <vlvete1990@yandex.ru>
2025-11-15 23:49:59 +0300

    feature: add GameSession

---
+++ Assets/PixelCrew/Model/GameSession.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace PixelCrew.Model
+{
+    public class GameSession: MonoBehaviour
+    {
+        [SerializeField] private PlayerData _data;
+        public PlayerData Data => _data;
+    }
+}
