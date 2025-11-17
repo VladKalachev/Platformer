bc2e219f43f4b7cdbbe351632f4c571d35df0248
Vlad <vlvete1990@yandex.ru>
2025-11-18 00:04:24 +0300

    feature: add CameraStateController

---
+++ Assets/PixelCrew/Components/ShowTargetComponent.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class ShowTargetComponent: MonoBehaviour
+    {
+        [SerializeField] private Transform _transform;
+
+        public void ShowTarget()
+        {
+            
+        }
+    }
+}
