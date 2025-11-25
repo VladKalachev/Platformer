dd18ea32c23714f62b78ff362c8c497ea4e822de
Vlad <vlvete1990@yandex.ru>
2025-11-25 23:36:47 +0300

    feature: add AgroToHero fn

--- Assets/PixelCrew/Creatures/MobAI.cs
+++ Assets/PixelCrew/Creatures/MobAI.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using PixelCrew.Components;
 using UnityEngine;
 
 namespace PixelCrew.Creatures
@@ -8,11 +10,54 @@ namespace PixelCrew.Creatures
         [SerializeField] private LayoutCheck _vision;
         [SerializeField] private LayoutCheck _canAttack;
 
+        [SerializeField] private float _alarmDelay = 0.5f;
         private Coroutine _current;
+        private GameObject _target;
+
+        private SpawnListComponent _particles;
+
+        private void Awake()
+        {
+            _particles = GetComponent<SpawnListComponent>();
+        }
 
         private void Start()
         {
+            StartState(Patrolling());
+        }
+
+        public void OnHeroInVision(GameObject go)
+        {
+            _target = go;
             
+            StartState(AgroToHero());
+        }
+
+        private IEnumerator AgroToHero()
+        {
+            _particles.Spawn("Exclamation");
+            yield return new WaitForSeconds(_alarmDelay);
+            StartState(GoToHero());
+        }
+
+        private IEnumerator GoToHero()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IEnumerator Patrolling()
+        {
+            yield return null;
+        }
+
+        private void StartState(IEnumerator coroutine)
+        {
+            if (_current != null)
+            {
+                StopCoroutine(_current);
+            }
+            
+            _current = StartCoroutine(coroutine);
         }
     }
 }
