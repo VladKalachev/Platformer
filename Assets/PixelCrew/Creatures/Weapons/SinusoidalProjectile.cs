11947eebb80d3fcd15858640401b141e39dd91bd
Vlad <vlvete1990@yandex.ru>
2025-12-10 00:41:53 +0300

    feature: add BaseProjectile

--- Assets/PixelCrew/Creatures/Weapons/SinusoidalProjectile.cs
+++ Assets/PixelCrew/Creatures/Weapons/SinusoidalProjectile.cs
@@ -5,7 +5,10 @@ namespace PixelCrew.Creatures.Weapons
 {
     public class SinusoidalProjectile: BaseProjectile
     {
+        [SerializeField] private float _frequency = 1f;
+        [SerializeField] private float _amplitude = 1f;
         private float _originalY;
+        private float _time;
         
         protected override void Start()
         {
@@ -15,9 +18,10 @@ namespace PixelCrew.Creatures.Weapons
         private void FixedUpdate()
         {
             var position = Rigidbody.position;
-            position.x = Direction * _speed;
-            position.y = _originalY + Mathf.Sin(Time.time);
+            position.x += Direction * _speed;
+            position.y = _originalY + Mathf.Sin(_time * _frequency) * _amplitude;
             Rigidbody.MovePosition(position);
+            _time += Time.fixedDeltaTime;
         }
     }
 }
