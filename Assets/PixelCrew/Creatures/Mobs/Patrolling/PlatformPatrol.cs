f742baf56868551fe8eed701d6918c60d7f7eb20
Vlad <vlvete1990@yandex.ru>
2025-12-29 00:21:43 +0300

    feature: add PlatformPatrole

--- Assets/PixelCrew/Creatures/Mobs/Patrolling/PlatformPatrol.cs
+++ Assets/PixelCrew/Creatures/Mobs/Patrolling/PlatformPatrol.cs
@@ -1,12 +1,32 @@
 using System.Collections;
+using PixelCrew.Components.ColliderBased;
+using UnityEngine;
 
 namespace PixelCrew.Creatures.Mobs.Patrolling
 {
     public class PlatformPatrol: Patrol
     {
+        [SerializeField] private LayoutCheck _groundCheck;
+        [SerializeField] private int _direction;
+        [SerializeField] private Creature _creature;
+        
         public override IEnumerator DoPatrol()
         {
-            yield return null;
+            while (enabled)
+            {
+                if (_groundCheck.IsTouchingLayer)
+                {
+                    _creature.SetDirection(new Vector2(_direction, 0));
+                }
+                else
+                {
+                    _direction = -_direction;
+                    _creature.SetDirection(new Vector2(_direction, 0));
+                }
+                
+                yield return null;
+            }
+           
         }
     }
 }
