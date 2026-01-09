f044a655079d8f308ad505969381916464325a83
Vlad <vlvete1990@yandex.ru>
2026-01-09 23:25:54 +0300

    feature: Add Settings Window

--- Assets/PixelCrew/Model/Data/Properties/PersistentProperty.cs
+++ Assets/PixelCrew/Model/Data/Properties/PersistentProperty.cs
@@ -5,6 +5,8 @@ namespace PixelCrew.Model.Data.Properties
     public abstract class PersistentProperty<TPropertyType>
     {
         [SerializeField] private TPropertyType _value;
+        private TPropertyType _stored;
+        
         private TPropertyType _defaultValue;
         
         public delegate void OnPropertyChanged(TPropertyType newValue, TPropertyType oldValue);
@@ -18,26 +20,34 @@ namespace PixelCrew.Model.Data.Properties
 
         public TPropertyType Value
         {
-            get => _value;
+            get => _stored;
             set
             {
-                var isEqual = _value.Equals(value);
+                var isEqual = _stored.Equals(value);
                 if (isEqual) return;
                 
                 var oldValue = _value;
                 Write(value);
-                    _value = value;
+                _stored = _value = value;
                     
-                    OnChanged?.Invoke(oldValue, value);
+                OnChanged?.Invoke(oldValue, value);
             }
         }
 
         protected void Init()
         {
-            _value = Read(_defaultValue);
+           _stored = _value = Read(_defaultValue);
         }
         
         protected abstract void Write(TPropertyType value);
         protected abstract TPropertyType Read(TPropertyType defaultValue);
+        
+        public void Validate()
+        {
+            if (!_stored.Equals(_value))
+            {
+                Value = _value;
+            }
+        }
     }
 }
--- Assets/PixelCrew/UI/Settings/SettingsWindow.cs
+++ Assets/PixelCrew/UI/Settings/SettingsWindow.cs
@@ -1,7 +1,15 @@
+using PixelCrew.Model.Data;
+
 namespace PixelCrew.UI.Settings
 {
     public class SettingsWindow: AnimatedWindow
     {
-        
+        protected override void Start()
+        {
+            base.Start();
+
+            // GameSettings.I.Music;
+
+        }
     }
 }
