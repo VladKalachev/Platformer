d48e0b12280da203a0ef11de1aec65858b50f6a1
Vlad <vlvete1990@yandex.ru>
2026-01-09 22:43:17 +0300

    feature: add PersistentProperty

--- Assets/PixelCrew/Model/Data/Properties/PersistentProperty.cs
+++ Assets/PixelCrew/Model/Data/Properties/PersistentProperty.cs
@@ -9,9 +9,35 @@ namespace PixelCrew.Model.Data.Properties
         
         public delegate void OnPropertyChanged(TPropertyType newValue, TPropertyType oldValue);
 
-        public event OnPropertyChanged onChanged;
+        public event OnPropertyChanged OnChanged;
+
+        public PersistentProperty(TPropertyType defaultValue)
+        {
+            _defaultValue = defaultValue;
+        }
+
+        public TPropertyType Value
+        {
+            get => _value;
+            set
+            {
+                var isEqual = _value.Equals(value);
+                if (isEqual) return;
+                
+                var oldValue = _value;
+                Write(value);
+                    _value = value;
+                    
+                    OnChanged?.Invoke(oldValue, value);
+            }
+        }
+
+        protected void Init()
+        {
+            _value = Read(_defaultValue);
+        }
         
         protected abstract void Write(TPropertyType value);
-        protected abstract TPropertyType Read();
+        protected abstract TPropertyType Read(TPropertyType defaultValue);
     }
 }
