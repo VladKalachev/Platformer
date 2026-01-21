156e77c117f8f14077fd5731334b62226434817c
Vlad <vlvete1990@yandex.ru>
2026-01-21 18:20:47 +0300

    feature: add DialogData

--- Assets/PixelCrew/Components/Dialogs/Editor/ShowDialogComponentEditor.cs
+++ Assets/PixelCrew/Components/Dialogs/Editor/ShowDialogComponentEditor.cs
@@ -1,5 +1,6 @@
 using System;
 using PixelCrew.Model.Definitions;
+using PixelCrew.Utils.Editor;
 using UnityEditor;
 
 namespace PixelCrew.Components.Dialogs.Editor
@@ -16,9 +17,19 @@ namespace PixelCrew.Components.Dialogs.Editor
         public override void OnInspectorGUI()
         {
             EditorGUILayout.PropertyField(_modeProperty);
-            
-            // _modeProperty.animationCurveValue.
-            
+         
+            if (_modeProperty.GetEnum(out ShowDialogComponent.Mode mode))
+            {
+                switch (mode)
+                {
+                    case ShowDialogComponent.Mode.Bound:
+                        EditorGUILayout.PropertyField(serializedObject.FindProperty("_bound"));
+                        break;
+                    case ShowDialogComponent.Mode.External :
+                        EditorGUILayout.PropertyField(serializedObject.FindProperty("_external"));
+                        break;
+                }     
+            }
             
             serializedObject.ApplyModifiedProperties();
         }
--- Assets/PixelCrew/Components/Dialogs/ShowDialogComponent.cs
+++ Assets/PixelCrew/Components/Dialogs/ShowDialogComponent.cs
@@ -1,5 +1,7 @@
+using System;
 using PixelCrew.Model.Data;
 using PixelCrew.Model.Definitions;
+using PixelCrew.UI.Hud.Dialogs;
 using UnityEngine;
 
 namespace PixelCrew.Components.Dialogs
@@ -9,6 +11,33 @@ namespace PixelCrew.Components.Dialogs
         [SerializeField] private Mode _mode;
         [SerializeField] private DialogData _bound;
         [SerializeField] private DialogDef _external;
+
+        private DialogBoxController _dialogBox;
+        
+        public void Show()
+        {
+            if (_dialogBox == null)
+                _dialogBox = FindObjectOfType<DialogBoxController>();
+            
+            _dialogBox.ShowDialog(Data);
+        }
+
+        public DialogData Data
+        {
+            get
+            {
+                switch (_mode)
+                {
+                    case Mode.Bound:
+                        return _bound;
+                    case Mode.External:
+                        return _external.Data;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
 
         public enum Mode
         {
