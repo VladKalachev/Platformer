20eeac8f0815422c14278a9670a8bf5a05213ba6
Vlad <vlvete1990@yandex.ru>
2026-01-27 21:23:56 +0300

    feature: add OptionDialogController

--- Assets/PixelCrew/UI/Hud/Dialogs/OptionDialogController.cs
+++ Assets/PixelCrew/UI/Hud/Dialogs/OptionDialogController.cs
@@ -1,4 +1,5 @@
 using System;
+using PixelCrew.UI.Widgets;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -7,13 +8,30 @@ namespace PixelCrew.UI.Hud.Dialogs
 {
     public class OptionDialogController : MonoBehaviour
     {
+        [SerializeField] private GameObject _content;
         [SerializeField] private Text _contentText;
-        [SerializeField] private Transform _container;
+        [SerializeField] private Transform _optionsContainer;
         [SerializeField] private OptionItemWidget _prefab;
-        
+
+        private DataGroup<OptionData, OptionItemWidget> _dataGroup;
+
+        private void Start()
+        {
+            _dataGroup = new DataGroup<OptionData, OptionItemWidget>(_prefab, _optionsContainer);
+        }
+
+        public void OnOptionsSelected(OptionData selectedOption)
+        {
+            selectedOption.OnSelect.Invoke();
+            _content.SetActive(false);
+        }
+
         public void Show(OptionDialogData data)
         {
+            _content.SetActive(true);
             _contentText.text = data.DialogText;
+            
+            _dataGroup.SetData(data.Options);
         }
     }
     
--- Assets/PixelCrew/UI/Hud/Dialogs/OptionItemWidget.cs
+++ Assets/PixelCrew/UI/Hud/Dialogs/OptionItemWidget.cs
@@ -1,18 +1,19 @@
 using System;
+using PixelCrew.UI.Widgets;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace PixelCrew.UI.Hud.Dialogs
 {
-    public class OptionItemWidget : MonoBehaviour
+    public class OptionItemWidget : MonoBehaviour, IItemRender<OptionData>
     {
         [SerializeField] private Text _lable;
         [SerializeField] private SelectOption _onSelect;
         
         private OptionData _data;
 
-        public void SetData(OptionData data)
+        public void SetData(OptionData data, int index)
         {
             _data = data;
             _lable.text = data.Text;
