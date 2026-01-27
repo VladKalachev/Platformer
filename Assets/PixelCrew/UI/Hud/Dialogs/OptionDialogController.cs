ae1f3a310679ff6e3a042c0598f9e74cdd76887b
Vlad <vlvete1990@yandex.ru>
2026-01-27 20:36:22 +0300

    feature: add ShowOptionsComponent

--- Assets/PixelCrew/UI/Hud/Dialogs/OptionDialogController.cs
+++ Assets/PixelCrew/UI/Hud/Dialogs/OptionDialogController.cs
@@ -1,12 +1,17 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 namespace PixelCrew.UI.Hud.Dialogs
 {
     public class OptionDialogController : MonoBehaviour
     {
-        public void SetData(OptionDialogData data)
+        [SerializeField] private Text _contentText;
+        [SerializeField] private Transform _container;
+        [SerializeField] private Prefab
+        
+        public void Show(OptionDialogData data)
         {
             
         }
